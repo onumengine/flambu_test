bb267d11e8da5e044d58a326f9880bd5b29e1c25
onumengine <onumdev@gmail.com>
2021-10-22 08:03:51 +0100

    rename the DogBreedEntity

--- lib/features/dogs/domain/entities/dog_breed.dart
+++ lib/features/dogs/domain/entities/dog_breed.dart
@@ -1,9 +1,9 @@
 import 'package:equatable/equatable.dart';
 
-class DogBreed extends Equatable {
+class DogBreedEntity extends Equatable {
   final String name;
 
-  const DogBreed(this.name);
+  const DogBreedEntity(this.name);
 
   @override
   List<Object?> get props => [name];
