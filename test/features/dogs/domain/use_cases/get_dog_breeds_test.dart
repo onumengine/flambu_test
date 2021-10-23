e24b1f099ad0bd764fa5a61a06dab070e8dfe50d
onumengine <onumdev@gmail.com>
2021-10-23 06:44:29 +0100

    clear the dog breed use case and repository tests

--- test/features/dogs/domain/use_cases/get_dog_breeds_test.dart
+++ test/features/dogs/domain/use_cases/get_dog_breeds_test.dart
@@ -1,5 +1,4 @@
 import 'package:dartz/dartz.dart';
-import 'package:flambu_test/features/dogs/domain/entities/dog_breed.dart';
 import 'package:flambu_test/features/dogs/domain/repositories/dogs_list_repository.dart';
 import 'package:flambu_test/features/dogs/domain/use_cases/get_dog_breeds.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -7,27 +6,4 @@ import 'package:mockito/mockito.dart';
 
 class MockDogsListRepository extends Mock implements DogsListRepository {}
 
-void main() {
-  late MockDogsListRepository? dogsListRepository;
-  late GetDogBreedsUseCase? useCase;
-
-  final dogBreeds = [
-    DogBreedEntity('Pit bull'),
-  ];
-
-  setUp(() {
-    dogsListRepository = MockDogsListRepository();
-    useCase = GetDogBreedsUseCase(dogsListRepository: dogsListRepository!);
-  });
-
-  test('UseCase returns a list of dog breeds', () async {
-    when(dogsListRepository?.getDogBreeds())
-        .thenAnswer((_) async => Right(dogBreeds));
-
-    final result = await useCase?.call();
-
-    expect(result, Right(dogBreeds).value[0]);
-
-    verify(useCase?.dogsListRepository.getDogBreeds());
-  });
-}
+void main() {}
