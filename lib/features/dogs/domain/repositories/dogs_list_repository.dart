126f76374da748a4080a8e36d82a60cccefd339d
onumengine <onumdev@gmail.com>
2021-10-23 06:41:55 +0100

    change the type of the DogsListRepository's success object

--- lib/features/dogs/domain/repositories/dogs_list_repository.dart
+++ lib/features/dogs/domain/repositories/dogs_list_repository.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flambu_test/core/error/failure.dart';
-import 'package:flambu_test/features/dogs/domain/entities/dog_breed.dart';
+import 'package:flambu_test/features/dogs/domain/entities/dog_breed_list.dart';
 
 abstract class DogsListRepository {
-  Future<Either<Failure, List<DogBreedEntity>>> getDogBreeds();
+  Future<Either<Failure, DogBreedListEntity>> getDogBreeds();
 }
--- lib/features/dogs/domain/use_cases/get_dog_breeds.dart
+++ lib/features/dogs/domain/use_cases/get_dog_breeds.dart
@@ -1,16 +1,16 @@
 import 'package:dartz/dartz.dart';
 import 'package:flambu_test/core/error/failure.dart';
-import 'package:flambu_test/features/dogs/domain/entities/dog_breed.dart';
+import 'package:flambu_test/features/dogs/domain/entities/dog_breed_list.dart';
 import 'package:flambu_test/features/dogs/domain/repositories/dogs_list_repository.dart';
 import 'package:flambu_test/features/main/domain/use_cases/use_case.dart';
 
-class GetDogBreedsUseCase implements UseCase<List<DogBreedEntity>> {
+class GetDogBreedsUseCase implements UseCase<DogBreedListEntity> {
   final DogsListRepository dogsListRepository;
 
   GetDogBreedsUseCase({required this.dogsListRepository});
 
   @override
-  Future<Either<Failure, List<DogBreedEntity>>> call() async {
-    return await dogsListRepository.getDogBreeds();
+  Future<Either<Failure, DogBreedListEntity>> call() {
+    return dogsListRepository.getDogBreeds();
   }
 }
