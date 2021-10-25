fc557752031f1222b67ae45d2f636ddfaaefd5cb
onumengine <onumdev@gmail.com>
2021-10-25 01:15:56 +0100

    change the right success type of the repo to a DogImageLinkEntity

--- lib/features/dogs/domain/repositories/random_dog_image_repository.dart
+++ lib/features/dogs/domain/repositories/random_dog_image_repository.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flambu_test/core/error/failure.dart';
+import 'package:flambu_test/features/dogs/domain/entities/dog_image_link.dart';
 
 abstract class RandomDogImageRepository {
-  Future<Either<Failure, String>> getRandomDogImageLink();
+  Future<Either<Failure, DogImageLinkEntity>> getRandomDogImageLink();
 }
