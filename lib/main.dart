d29f9eeb07af161c800d05208fdcedfab9cbf1a1
onumengine <onumdev@gmail.com>
2021-10-26 22:44:55 +0100

    wrap the HomePage in a MultiBlocProvider of the two Blocs

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
-import 'package:flambu_test/features/dogs/presentation/bloc/bloc.dart';
+import 'package:flambu_test/features/dogs/presentation/logic_holders/breed_list_bloc/bloc.dart';
 import 'package:flambu_test/features/dogs/presentation/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flambu_test/injection_container.dart' as injector;
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'features/dogs/presentation/logic_holders/dog_image_bloc/bloc.dart';
 
 void main() {
   injector.init();
@@ -19,9 +21,16 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: BlocProvider(
-        create: (_) => injector.serviceLocator<BreedListBloc>(),
-        child: const HomePage(),
+      home: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (_) => injector.serviceLocator<BreedListBloc>(),
+          ),
+          BlocProvider(
+            create: (_) => injector.serviceLocator<DogImageBloc>(),
+          ),
+        ],
+        child: HomePage(),
       ),
     );
   }
